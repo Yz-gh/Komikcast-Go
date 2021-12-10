214173f46e78394f57e955dac253f0e7026d8363
Yz-gh <75504671+Yz-gh@users.noreply.github.com>
2021-12-10 12:57:43 +0700

    Create example.go

---
+++ example.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "fmt"
+
+    "github.com/Yz-gh/Komikcast-Go"
+)
+
+func main(){
+    fmt.Println("=====[ Search ]======")
+    s, _ := komikcast.SearchComic("omniscient")
+    for _, cd := range s{ fmt.Printf("%+v\n", cd) }
+    cid := s[0].LinkID
+    fmt.Println()
+
+    fmt.Println("=====[ Get Comic ]=====")
+    fmt.Println("Comic id to get:", cid)
+    c, _ := komikcast.GetComic(cid)
+    fmt.Printf("%+v\nChapter list:\n", c)
+    for _, d := range c.ListChapter{ fmt.Printf("%+v\n", d) }
+    fmt.Println()
+
+    fmt.Println("=====[ Read Comic ]=====")
+    tr := c.ListChapter[0].LinkID
+    fmt.Println("Chapter id to read:", tr)
+    rc, _ := komikcast.ReadComic(tr)
+    for _, img := range rc.Images{ fmt.Println(img) }
+}
